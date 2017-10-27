4d8d084cb6ed67eb7077ac933a0921c57c19c9eb
Simon <simon.crowther95@hotmail.co.uk>
2017-10-27 12:44:17 +0100

    Update of Menu_Class.h
    Changed description to be accurate of version number

--- Touch_Screen/Touch_Screen/Menu_Class.h
+++ Touch_Screen/Touch_Screen/Menu_Class.h
@@ -18,6 +18,8 @@
       -------         ----            ------          -----------
       1.0            24/10/17         S.Crowther      Initial version
       1.1            25/10/17         S.Crowther      Added Namespace with data structure for presets 
+      1.2            27/10/17         S.Crowther      Added changes to main functions created in the namespace presets, swapped
+                                                      the longer functions with shorter and more useful ones
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 */
 
